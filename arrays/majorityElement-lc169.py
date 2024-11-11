025d9331932100f7bebb1d8d7b93234bafa84329
Hyunsang Cho <hyunsang4072@gmail.com>
2024-11-11 10:37:54 -0800

    solved majorityElement-lc169 problem

---
+++ arrays/majorityElement-lc169.py
@@ -0,0 +1,34 @@
+def majorityElement(self, nums: List[int]) -> int:
+    # hashMap approach
+    # seen = {}
+
+    # for i in range(len(nums)):
+    #     val = nums[i]
+    #     # if val not in seen:
+    #     #     seen[val] = 1
+    #     # seen[val] += 1
+    #     seen[val] = seen.setdefault(val, 0) + 1
+    
+    # th = len(nums) // 2
+
+    # for el in seen:
+    #     if seen[el] > th:
+    #         return el
+
+    # optimal approach --> Moore's voting algorithm
+    # iterate through array once
+    # when iterating, keep track of val that has not been counted out yet
+    # c is to keep track of whether this current val is not outnumbered by other elements or not
+    # once c has reached for current val, it has to be replaced by next element in the array
+    val, c = 0, 0
+    
+    for i in range(len(nums)):
+        if c == 0:
+            val = nums[i]
+            c += 1
+        elif val != nums[i]:
+            c -= 1
+        else:
+            c += 1
+    
+    return val
