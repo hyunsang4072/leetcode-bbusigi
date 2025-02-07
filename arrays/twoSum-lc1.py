9b43efe43d00c86f053743dee98f9c77a7049c3e
Hyunsang Cho <hyunsang4072@gmail.com>
2025-02-07 00:27:02 -0800

    review lc1-twoSum problem

--- arrays/twoSum-lc1.py
+++ arrays/twoSum-lc1.py
@@ -17,20 +17,34 @@ def twoSum(self, nums: list[int], target: int) -> list[int]:
     # SC = O(1)
     # 1. sort the array
 
+    # for i in range(len(nums)):
+    #     nums[i] = (nums[i], i)
+    # nums.sort()
+
+    # # 2. greedy approach to find two indices
+    # # two ptrs: left at the beginning; right at the end
+    # #[(2, 1), (3, 0), (4, 2)]
+
+    # l, r = 0, len(nums) - 1
+    # while l != r:
+    #     currSum = nums[l][0] + nums[r][0]
+    #     if currSum == target:
+    #         return [nums[l][1], nums[r][1]]
+    #     elif currSum < target:
+    #         l += 1
+    #     elif currSum > target:
+    #         r -= 1
+
+    # Optimal hashMap soln.
+    # TC: O(n)
+    # Space: O(n)
+    hashMap = {}
+
     for i in range(len(nums)):
-        nums[i] = (nums[i], i)
-    nums.sort()
+        n = nums[i]
+        diff = target - n
 
-    # 2. greedy approach to find two indices
-    # two ptrs: left at the beginning; right at the end
-    #[(2, 1), (3, 0), (4, 2)]
-
-    l, r = 0, len(nums) - 1
-    while l != r:
-        currSum = nums[l][0] + nums[r][0]
-        if currSum == target:
-            return [nums[l][1], nums[r][1]]
-        elif currSum < target:
-            l += 1
-        elif currSum > target:
-            r -= 1
+        if diff in hashMap:
+            return [hashMap[diff], i]
+        
+        hashMap[n] = i
