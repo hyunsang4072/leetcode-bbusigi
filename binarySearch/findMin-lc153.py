ecbce50b1e68edc537a191ee87162c3a12af1c3d
Hyunsang Cho <hyunsang4072@gmail.com>
2025-02-21 15:00:22 -0800

    solved lc153-findMin problem

---
+++ binarySearch/findMin-lc153.py
@@ -0,0 +1,17 @@
+def findMin(self, nums: list[int]) -> int:
+    l, r = 0, len(nums) - 1
+    res = nums[0]
+
+    while l <= r:
+        if nums[l] < nums[r]:
+            res = min(res, nums[l])
+            break
+        
+        m = (l + r) // 2
+        res = min(res, nums[m])
+        if nums[r] >= nums[m]:
+            r = m - 1
+        else:
+            l = m + 1
+    
+    return res
