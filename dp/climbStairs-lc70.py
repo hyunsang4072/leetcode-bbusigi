e10f2c508650f1112287a08964c2da16e91d449d
Hyunsang Cho <hyunsang4072@gmail.com>
2025-03-04 22:39:31 -0800

    revisit lc70-climbStairs problem

--- dp/climbStairs-lc70.py
+++ dp/climbStairs-lc70.py
@@ -1,52 +1,40 @@
 def climbStairs(self, n: int) -> int:
-    # recursion w/ memoization
-    # time: O(n)
-    # space: O(n)
-    # memo = {}
-
-    # def helper(i, n, m):
-    #     if i > n:
-    #         return 0
-    #     if n == i:
-    #         return 1
-        
-    #     if (i, n) in m:
-    #         return m[(i, n)]
-        
-    #     dp = helper(i + 1, n, m) + helper(i + 2, n, m)
-
-    #     m[(i, n)] = dp
-
-    #     return dp
-
-    # return helper(0, n, memo)
-
-    ################################################
-    # DP w/o space optimization
-    # time: O(n)
-    # space: O(n)
-    if n <= 2: # 1 way at n=1; 2 ways at n=2
-        return n
+    # at least for this problem, where the maximum step
+    # we can take is 2, the first and second will always have
+    # default value of two
+    first, second = 1, 1
     
-    # initialize dp array(1-D) bottom-up approach
-    dp = [0] * (n) # [0, ... , 0]
-    dp[0] = 1
-    dp[1] = 2
-
-    for i in range(2, n): # i = 3, ..., n
-        dp[i] = dp[i-1] + dp[i-2]
+    # e.g., n = 5
+    # visualize an arr like this: [0, 0, 0, 0, 0, 0] since 0 up to 5
+    # we need to think about our base cases so we start from the end of an arr
+    # i.e., [0, 0, 0, 0, 1, 1] where last two indices contain second and first
+    # start building up the arr woking towards the beginning of our arr
+    # i.e., [0, 0, 0, 2, 1, 1]
+    # i.e., [0, 0, 3, 2, 1, 1]
+    # i.e., [0, 5, 3, 2, 1, 1]
+    # i.e., [8, 5, 3, 2, 1, 1]
+    # notice how we don't really need the last step where we computed 8
+    # so we can just do this in (n - 1) iterations!
+    for i in range(n - 1):
+        temp = second
+        second = first + second
+        first = temp
+        # or w/o using temp variable can be done as follows:
+        # second = first + second
+        # first = second - first
     
-    return dp[n-1]
+    return second
 
-    ################################################
-    # DP w/ space optimization == optimal soln.
-    # time: O(n)
-    # space: O(1)
-    # one, two = 1, 1
-
-    # for i in range(n-1):
-    #     temp = two
-    #     two = one + two
-    #     one = temp
+    ####### dp w/ bottom-up #######
+    # TC: O(n)
+    # SC: O(n)
+    # if n <= 2:
+    #     return n
     
-    # return two
+    # dp = [0] * (n + 1) # e.g., [0, 0, 0, 0]
+    # dp[1], dp[2] = 1, 2 # e.g., [0, 1, 2, 0]
+
+    # for i in range(3, n + 1):
+    #     dp[i] = dp[i - 2] + dp[i - 1] # e.g., if n==4: [0, 1, 2, 3, 5]
+    
+    # return dp[n]
