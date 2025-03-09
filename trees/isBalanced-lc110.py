27ad8a2bf286c77c39b39cb9018fada31b23d66c
Hyunsang Cho <hyunsang4072@gmail.com>
2025-03-08 18:51:49 -0800

    solved lc110-isBalanced problem

---
+++ trees/isBalanced-lc110.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    def dfs(root):
+        if not root:
+            return 0
+        
+        left = dfs(root.left)
+        right = dfs(root.right)
+
+        # if unbalanced tree, return -1
+        if left == -1 or right == -1 or abs(left - right) > 1:
+            return -1
+        
+        # in normal(balanced) case, return height of current tree
+        return 1 + max(left, right)
+
+    # if return -1, somewhere in tree not balanced portion
+    return dfs(root) != -1
