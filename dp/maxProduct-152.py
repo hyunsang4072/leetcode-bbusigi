c596eb932b3adcd5ace49c350b92a8e3c03cf5d4
Hyunsang Cho <hyunsang4072@gmail.com>
2025-04-11 13:24:11 -0700

    solved lc152-maxProduct problem

---
+++ dp/maxProduct-152.py
@@ -0,0 +1,40 @@
+def maxProduct(self, nums: List[int]) -> int:
+    # Kadane's Algo
+    # keep the curMax, curMin to update future max values
+    # TC: O(n)
+    # SC: O(1)
+    res = nums[0]
+    curMin, curMax = 1, 1
+
+    for i in range(len(nums)):
+        temp = curMax * nums[i]
+        curMax = max(nums[i] * curMax, nums[i] * curMin, nums[i])
+        # temp is needed to store the old curMax * nums[i] value
+        curMin = min(temp, nums[i] * curMin, nums[i])
+        # we don't want to consider curMin when calculating res
+        res = max(res, curMax)
+    
+    return res
+
+    ####### brute force -> TLE #######
+    # if len(nums) == 1:
+    #     return nums[0]
+    # res = 0
+
+    # def dfs(i, j):
+    #     nonlocal res
+
+    #     if j == len(nums):
+    #         return
+        
+    #     prod = 1
+    #     for ind in range(i, j + 1):
+    #         prod *= nums[ind]
+    #     res = max(res, prod)
+
+    #     dfs(i + 1, j + 1)
+    #     dfs(i, j + 1)
+    
+    # dfs(0, 0)
+
+    # return res
