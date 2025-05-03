c46e246c6fcd703561f04bfc1d45ee553405e44d
Hyunsang Cho <hyunsang4072@gmail.com>
2025-05-02 18:23:18 -0700

    solved lc261-validTree problem

---
+++ graph/validTree-lc261.py
@@ -0,0 +1,31 @@
+def validTree(self, n: int, edges: List[List[int]]) -> bool:
+    visited = set()
+    # generate adjacency list
+    adj = defaultdict(list)
+    for n1, n2 in edges:
+        adj[n1].append(n2)
+        adj[n2].append(n1)
+    
+    # check1: #Nodes = #Edges + 1 <- Tree property
+    if n != len(edges) + 1:
+        return False
+    
+    # check2: are all nodes connected == is our graph a connected graph?
+    def dfs(i, prev):
+        if i in visited:
+            return False
+        
+        visited.add(i)
+
+        for nei in adj[i]:
+            if nei == prev:
+                continue
+            if not dfs(nei, i): # current node i becomes prev for node nei
+                return False
+        
+        return True
+    
+    # use -1 for base case since
+    # -1 is never visited == -1 will never be prev node of any nodes
+    return dfs(0, -1) and len(visited) == n
+    
